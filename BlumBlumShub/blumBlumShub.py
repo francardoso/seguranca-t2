d991f549f020a0c6297cde1f4088200248ed90e0
Fran Cardoso <fran.adalid@hotmail.com>
2019-09-16 11:13:07 -0300

    blumblum algorith good

--- BlumBlumShub/blumBlumShub.py
+++ BlumBlumShub/blumBlumShub.py
@@ -78,16 +78,62 @@ class BlumBlumShub(object):
             result = (result << 1) | (self.state&1)
         
         return result    
+    
+    import random
+ 
+def is_Prime(n):
+    """
+    Miller-Rabin primality test.
+ 
+    A return value of False means n is certainly not prime. A return value of
+    True means n is very likely a prime.
+    """
+    if n!=int(n):
+        return False
+    n=int(n)
+    #Miller-Rabin test for prime
+    if n==0 or n==1 or n==4 or n==6 or n==8 or n==9:
+        return False
+ 
+    if n==2 or n==3 or n==5 or n==7:
+        return True
+    s = 0
+    d = n-1
+    while d%2==0:
+        d>>=1
+        s+=1
+    assert(2**s * d == n-1)
+ 
+    def trial_composite(a):
+        if pow(a, d, n) == 1:
+            return False
+        for i in range(s):
+            if pow(a, 2**i * d, n) == n-1:
+                return False
+        return True  
+ 
+    for i in range(8):#number of trials 
+        a = random.randrange(2, n)
+        if trial_composite(a):
+            return False
+ 
+    return True  
 
 if __name__ == "__main__":
-    
     bbs = BlumBlumShub(128);
+    prime = 0
+    while is_Prime(prime) != True:
+       prime = bbs.next(4096)
+    else:
+        print(prime)
+
+   
         
     #print "Generating 10 numbers"
     
-    print("type: u")
-    print("numbit: 32")
-    print("count: 1")
+    # print("type: u")
+    # print("numbit: 32")
+    # print("count: 1")
     # for i in xrange (1):
     #     print(bbs.next(4096))
-    print(bbs.getPrime(32))
+    # print(bbs.getPrime(32))
